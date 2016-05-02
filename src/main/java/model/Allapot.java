475aed0f0350a1fc42d5f4dc1057b6aea93601fc
Balogh István Dávid <pistiz7@gmail.com>
2016-05-02 17:50:01 +0200

    Alapok

---
+++ src/main/java/model/Allapot.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum Allapot
+{
+
+	hibás, sérült, megkímélt, sérülésmentes, újszerű, kitűnő
+
+}
---
+++ src/main/java/model/Uzemanyag.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum Uzemanyag {
+
+	benzin, dízel, hibrid, benzin_gáz, dízel_gáz, elektromos
+
+}
